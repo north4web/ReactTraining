d31d2368cec5aa9a91d18adb56873a34a3d77959
north4web <69797932+north4web@users.noreply.github.com>
2020-12-10 14:39:53 +0100

    body component improved

--- src/Home/Body/Body.js
+++ src/Home/Body/Body.js
@@ -6,27 +6,67 @@ class Body extends React.Component {
         brand: 'Peugeot',
         model: '307cc',
         color: 'silver',
-        year: 2005
+        year: 2005,
+        backgroundColor: 'silver'
     }
 
-    changeColor = () => {
-        this.setState({ color: "red" });
+    changeCar = () => {
+        if (this.state.brand === 'Peugeot') {
+
+            this.setState({ brand: 'Audi', model: 'TT', year: 2015 });
+        } else {
+            this.setState({ brand: 'Peugeot', model: '307cc', year: 2005 });
+        }
+    }
+
+    // changeBackgroundColor = () => {
+    //     this.setState({ backgroundColor: 'pink' });
+    // }
+
+    updateBcgColor = (e) => {
+
+        this.setState({ backgroundColor: e.target.value })
     }
 
     render() {
+
         return (
 
             <div>
                 ------------------------------------------- <br />
-                From here down is Body <br /> 
-                <h2>My Car</h2>
+                From here down is Body <br /><br /><br />
 
-                <h4>My car is {this.state.brand}, {this.state.model}, color {this.state.color}, from {this.state.year} </h4>
-                <button
-                type = "button" 
-                onCLick = {this.changeColor}
-                >Change color</button>
+
+
+
+                <div style={{ width: '405px', height: '36px', backgroundColor: this.state.backgroundColor }}>
+
+                    <label style={{ color: 'darkblue', fontSize: '19px' }}>Type a Color</label> <input onChange={this.updateBcgColor} style={{ margineTop: '5px', border: '5px solid darkgray'}}/>
+
+
+                    {/* <button
+                    type="button"
+                    onClick={this.changeBackgroundColor}
+                >Change Background color</button> */}
+
+                    {/* <p>The color is: {this.state.backgroundColor}</p> */}
+                </div>
+                <div>
+                    
+                    <h4 style={{ color: 'darkblue' }}>{this.state.brand} {this.state.model}, <span style={{ color: this.state.backgroundColor, fontWeight: 'bold' }}> {this.state.backgroundColor}</span>, from {this.state.year} </h4>
+                    
+                    <button
+                        type="button"
+                        onClick={this.changeCar}
+                    >Change Car</button>
+                    <br /><br /><br />
+                </div>
+
+
             </div>
+
+
+
         )
     }
 
--- src/Home/Header/Header.js
+++ src/Home/Header/Header.js
@@ -8,9 +8,6 @@ class Header extends React.Component {
 
             <div>
 
-                
-
-
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
                     <a class="navbar-brand" href="#">React4</a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
